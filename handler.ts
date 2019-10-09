f0c69056f4ac324d866b4d6ede45875f4234d315
yukin01 <bump1105@gmail.com>
2019-10-09 15:13:26 +0900

    Check webhook body

--- handler.ts
+++ handler.ts
@@ -1,12 +1,17 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+// import * as AWS from 'aws-sdk'
+
+// const s3 = new AWS.S3()
 
 export const webhook: APIGatewayProxyHandler = async (event, _context) => {
+  const body = JSON.parse(event.body)
+  console.log(JSON.stringify(body, null, 2))
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
+      input: event
     }, null, 2),
   };
 }
